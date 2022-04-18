5d0041c4d075d9aec058d2bd959005dd269c0cce
abenoit <abenoit@student.42.fr>
2022-04-18 14:22:01 +0200

    modifications to case insensitive strcmp checks needed

--- src/str/ft_strcasecmp.c
+++ src/str/ft_strcasecmp.c
@@ -2,9 +2,19 @@
 /*Libc's strcoll is also case insensitive*/
 /*but takes locale into account*/
 
+static int	casecmp(int c1, int c2)	{
+	if (((c1 >= 'A' && c1 <= 'Z')
+		|| (c1 >= 'a' && c1 <= 'z'))
+		&& ((c2 >= 'A' && c2 <= 'Z')
+			|| (c2 >= 'a' && c2 <= 'z'))
+		&& (*s1 | 0b00100000) == (*s2 | 0b00100000))
+			return (1);
+	return (0);
+}
+
 int	ft_strcasecmp(const char *s1, const char *s2)	{
 	while (*s1 || *s2)	{
-		if ((*s1 | 0b00100000) != (*s2 | 0b00100000))
+		if (*s1 =! *s2 && !casecmp(*s1, *s2))
 			return (*s1 - *s2);
 		s1++;
 		s2++;
